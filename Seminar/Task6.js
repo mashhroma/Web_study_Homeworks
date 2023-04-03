0ba1e52d999bdc8c5c31c71275f131d9f9861a0d
Mashhroma <mashh@mail.ru>
2023-04-03 16:18:56 +0300

    homework 3

---
+++ Seminar/Task6.js
@@ -0,0 +1,16 @@
+// Функция getMaxEvenElement принимает массив с целыми числами, необходимо 
+// реализовать функцию так, чтобы она возвращала значение большего элемента массива, 
+// который записан в четном индексе включая 0.
+
+function getMaxEvenElement(arr) {
+    let max = arr[0];
+    for (let i = 2; i < arr.length; i += 2) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(getMaxEvenElement([5, 7, -1, 12, 3, 0])); // 5
+console.log(getMaxEvenElement([4, -12, 29, 6, 31, 92, -50])); // 31
